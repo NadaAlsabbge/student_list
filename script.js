6654a033e412d7cd7362422dbbfe666bc494442d
Nada Mabrouk <108991990+nada0mabrouk@users.noreply.github.com>
2022-09-07 15:59:36 -0700

    edite js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-// const student = document.getElementById("title")
-// const bottons = document.getElementById("btn")
-// const students = document.getElementById("appear")
-
-// function fun1() {
-//     students.innerHtml = student.value;
-// }
-// bottons.addEventListener('click', fun1)
 function myFunction() {
     var x = document.getElementById("title").value;
-    document.getElementById("appear").innerHTML += x;
+    document.getElementsByTagName("form")[0].innerHTML += `<div id="appear" class="appear">${x}</div>`;
 }
